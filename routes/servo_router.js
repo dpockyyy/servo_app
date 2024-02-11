7bcf2772ff0a9bc6acf3b2a74665b37417f4313a
Casey <yoshiplanet2092@gmail.com>
2024-02-11 16:36:03 +1100

    level 1 quests + some level 2 quests

---
+++ routes/servo_router.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const Servo = require('../models/servos.js')
+
+
+
+router.get('/api/stations/all', (req, res) => {
+    Servo.findAllServos()
+        .then(servos => res.json(servos))
+})
+
+router.get('/api/owners', (req, res) => {
+    Servo.findUniqueOwners()
+    .then(owners => res.json(owners))
+})
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -4,15 +4,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 9090
-const pg = require('pg')
+const servoRouter = require('./routes/servo_router.js')
 
 
 // Navigation
-app.use(express.static('client'))
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        MAPS_KEY: process.env.MAPS_KEY
+    })
+})
 
 app.get('/test', (req, res) => {
     res.send("It's working")
 })
+
+app.use(servoRouter)
 
 
 // Port
