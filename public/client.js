00e12671f89853be22923fc44c21a58d78566fdd
nikki <nikkiw.wu@gmail.com>
2024-02-12 16:12:06 +1100

    stats

--- public/client.js
+++ public/client.js
@@ -1,14 +1,46 @@
 let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+//   const map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 13,
+//     minZoom: 9,
+//   });
+//   const priceTag = document.createElement("div");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+//   priceTag.className = "price-tag";
+//   priceTag.textContent = "$2.5M";
+
+//   const marker = new AdvancedMarkerElement({
+//     map,
+//     position: { lat: 37.42, lng: -122.1 },
+//     content: priceTag,
+//   });
+// }
+
+// initMap();
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const map = new Map(document.getElementById("map"), {
+    center: { lat: 37.42, lng: -122.1 },
     zoom: 13,
     minZoom: 9,
+    mapId: "4504f8b37365c3d0",
+  });
+  const priceTag = document.createElement("div");
+
+  priceTag.className = "price-tag";
+  priceTag.textContent = "$2.5M";
+
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 37.42, lng: -122.1 },
+    content: priceTag,
   });
 }
 
 initMap();
-
