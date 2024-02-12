e95e722829e70a556aa36b450c8319cf58b4a62b
dpark <davidparkus04@gmail.com>
2024-02-13 08:37:33 +1100

    added map center location, polished some code

--- public/client.js
+++ public/client.js
@@ -1,42 +1,32 @@
-// let map;
+const mapCenterLat = document.querySelector('.map-ctr-lat')
+const mapCenterLng = document.querySelector('.map-ctr-lng')
 
-// async function initMap() {
-//   // Request needed libraries.
-//   const { Map } = await google.maps.importLibrary("maps");
-//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-//   const map = new Map(document.getElementById("map"), {
-//     center: { lat: 37.42, lng: -122.1 },
-//     zoom: 13,
-//     minZoom: 9,
-//     mapId: "4504f8b37365c3d0",
-//   });
-  
-//   const priceTag = document.createElement("div");
+// hardcoded for now, pulled it out as variables so I can set starting co-ords for map center.
+let mapStartCenterLat = -37.42
+let mapStartCenterLng = 144
 
-//   priceTag.className = "price-tag";
-//   priceTag.textContent = "$2.5M";
-
-//   const marker = new AdvancedMarkerElement({
-//     map,
-//     position: { lat: 37.42, lng: -122.1 },
-//     content: priceTag,
-//   });
-// }
-
-// initMap();
-
-let map;
+mapCenterLat.textContent = mapStartCenterLat
+mapCenterLng.textContent = mapStartCenterLng
 
 async function initMap() {
   // Request needed libraries.
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   const map = new Map(document.getElementById("map"), {
-    center: { lat: -37.42, lng: 144 },
+    center: { lat: mapStartCenterLat, lng: mapStartCenterLng },
     zoom: 13,
     minZoom: 9,
     mapId: "4504f8b37365c3d0",
   });
+
+  google.maps.event.addListener(map, "center_changed", function() {
+    var center = this.getCenter()
+    var latitude = center.lat()
+    var longitude = center.lng()
+   mapCenterLat.textContent = latitude.toFixed(2)
+   mapCenterLng.textContent = longitude.toFixed(2)
+  
+  })
   
   fetch('http://localhost:9090/api/stations/all')
   .then(response => response.json())
@@ -61,33 +51,35 @@ async function initMap() {
       });
   })
 )
+
 }
 
 initMap();
 
+
 window.addEventListener("load", () => {
-  clock();
+  clock()
   function clock() {
-    const today = new Date();
+    const today = new Date()
 
     // get time components
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
-    const seconds = today.getSeconds();
+    const hours = today.getHours()
+    const minutes = today.getMinutes()
+    const seconds = today.getSeconds()
 
     //add '0' to hour, minute & second when they are less 10
-    const hour = hours < 10 ? "0" + hours : hours;
-    const minute = minutes < 10 ? "0" + minutes : minutes;
-    const second = seconds < 10 ? "0" + seconds : seconds;
+    const hour = hours < 10 ? "0" + hours : hours
+    const minute = minutes < 10 ? "0" + minutes : minutes
+    const second = seconds < 10 ? "0" + seconds : seconds
 
     //make clock a 12-hour time clock
-    const hourTime = hour > 12 ? hour - 12 : hour;
+    const hourTime = hour > 12 ? hour - 12 : hour
 
     // if (hour === 0) {
     //   hour = 12;
     // }
     //assigning 'am' or 'pm' to indicate time of the day
-    const ampm = hour < 12 ? "AM" : "PM";
+    const ampm = hour < 12 ? "AM" : "PM"
 
     // get date components
     const day = today.getDay()
@@ -104,13 +96,13 @@ window.addEventListener("load", () => {
     }
     //get current date and time
     
-    const time = hourTime + ":" + minute + ":" + second + ampm;
+    const time = hourTime + ":" + minute + ":" + second + ampm
 
     //combine current date and time
-    const dateTime = dayName + " " + time;
+    const dateTime = dayName + " " + time
 
     //print current date and time to the DOM
-    document.getElementById("date-time").innerHTML = dateTime;
-    setTimeout(clock, 1000);
+    document.getElementById("date-time").innerHTML = dateTime
+    setTimeout(clock, 1000)
   }
-});
+})
