9a393ef3f28d095cc45501ef5d3ebd0419871b75
dpark <davidparkus04@gmail.com>
2024-02-12 14:16:08 +1100

    database schema + data import script

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE servos;
+
+CREATE TABLE servo_info (
+    id SERIAL PRIMARY KEY,
+    objectId INTEGER,
+    featureType TEXT NOT NULL,
+    description TEXT NOT NULL,
+    class TEXT NOT NULL,
+    fid TEXT,
+    name TEXT NOT NULL,
+    operational_status TEXT NOT NULL,
+    owner TEXT NOT NULL,
+    industryId TEXT,
+    address TEXT NOT NULL,
+    suburb TEXT NOT NULL,
+    state TEXT NOT NULL,
+    spatial_confidence TEXT,
+    revised TEXT,
+    comment TEXT,
+    latitude TEXT NOT NULL,
+    longitude TEXT NOT NULL
+);
+
+
